c9a8becdf9746244a6760a63356c345fa594251b
Michał Sośnicki <sosnicki.michal@gmail.com>
2016-03-14 08:08:55 +0100

    Initialize project

---
+++ src/main/java/pl/lodz/p/michalsosn/LampekApplication.java
@@ -0,0 +1,12 @@
+package pl.lodz.p.michalsosn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LampekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LampekApplication.class, args);
+	}
+}
---
+++ src/test/java/pl/lodz/p/michalsosn/LampekApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.lodz.p.michalsosn;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = LampekApplication.class)
+public class LampekApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
